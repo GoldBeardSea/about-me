bb5a3727d897f5c5a2e77a5bfa1901b1fb8ca1d0
GoldbeardSea <timbusch1@gmail.com>
2018-04-03 16:43:34 -0700

    Filled out the initial text in HTML, trying to establish some CSS

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ alert('Alright, it is time to play a game, please answer Yes/No or Y/n');
 var questionCat = prompt('Do I love cats?').toUpperCase();
 console.log(questionCat);
 if(questionCat === "YES" || questionCat === "Y") {
-  alert('That is correct');
+  alert('That is correct!');
 } else {
   alert('That is not correct!');
 }
@@ -13,7 +13,7 @@ if(questionCat === "YES" || questionCat === "Y") {
 var questionMusic = prompt('Do I make experimental music?').toUpperCase();
 console.log(questionMusic);
 if(questionMusic === "YES" || questionMusic === "Y") {
-  alert('That is correct');
+  alert('That is correct!');
 } else {
   alert('That is not correct!');
 }
@@ -21,7 +21,7 @@ if(questionMusic === "YES" || questionMusic === "Y") {
 var questionPeeps = prompt('Do I love eating peeps?').toUpperCase();
 console.log(questionPeeps);
 if(questionPeeps === "NO" || questionPeeps === "N") {
-  alert('That is correct');
+  alert('That is correct!');
 } else {
   alert('That is not correct!');
 }
@@ -29,15 +29,15 @@ if(questionPeeps === "NO" || questionPeeps === "N") {
 var questionLingual = prompt('Is english the only language that I speak?').toUpperCase();
 console.log(questionLingual);
 if(questionLingual === "NO" || questionLingual === "N") {
-  alert('That is correct');
+  alert('That is correct!');
 } else {
   alert('That is not correct!');
 } 
 
-var questionSeason = prompt('Do I hate summer as a season').toUpperCase();
+var questionSeason = prompt('Do I hate summer as a season?').toUpperCase();
 console.log(questionSeason);
 if(questionSeason === "YES" || questionSeason === "Y") {
-  alert('That is correct');
+  alert('That is correct!');
 } else {
   alert('That is not correct!');
 }
