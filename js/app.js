e4c6829df2551af37a2feba8d96576dcc5fbd4af
GoldbeardSea <timbusch1@gmail.com>
2018-04-04 18:30:06 -0700

    I added in the question tallying in its place

--- js/app.js
+++ js/app.js
@@ -1,48 +1,54 @@
 'use strict';
+
 var questionCorrect = 0
 
 console.log(questionCorrect)
-// alert('Alright, it is time to play a game, please answer Yes/No or Y/n');
+alert('Alright, it is time to play a game, please answer Yes/No or Y/n');
 
-// var questionCat = prompt('Do I love cats?').toUpperCase();
-// console.log(questionCat);
-// if(questionCat === "YES" || questionCat === "Y") {
-//   alert('Yes! I help foster and raise cats for adoption.');
-// } else {
-//   alert('Smooth move, ExLax! Goose egg on this one.');
-// }
+var questionCat = prompt('Do I love cats?').toUpperCase();
+console.log(questionCat);
+if(questionCat === "YES" || questionCat === "Y") {
+  questionCorrect++;
+  alert('Yes! I help foster and raise cats for adoption.');
+} else {
+  alert('Smooth move, ExLax! Goose egg on this one.');
+}
 
-// var questionMusic = prompt('Do I make experimental music?').toUpperCase();
-// console.log(questionMusic);
-// if(questionMusic === "YES" || questionMusic === "Y") {
-//   alert('I friggen love playing the WEIRD stuff!');
-// } else {
-//   alert('What do you think I am, the Monkees?!');
-// }
+var questionMusic = prompt('Do I make experimental music?').toUpperCase();
+console.log(questionMusic);
+if(questionMusic === "YES" || questionMusic === "Y") {
+  questionCorrect++;
+  alert('I friggen love playing the WEIRD stuff!');
+} else {
+  alert('What do you think I am, the Monkees?!');
+}
 
-// var questionPeeps = prompt('Do I love eating peeps?').toUpperCase();
-// console.log(questionPeeps);
-// if(questionPeeps === "NO" || questionPeeps === "N") {
-//   alert('Normal marshmallow is tasty enough, heck no to peeps!');
-// } else {
-//   alert('Get out of town! WRONG.');
-// }
+var questionPeeps = prompt('Do I love eating peeps?').toUpperCase();
+console.log(questionPeeps);
+if(questionPeeps === "NO" || questionPeeps === "N") {
+  questionCorrect++;
+  alert('Normal marshmallow is tasty enough, heck no to peeps!');
+} else {
+  alert('Get out of town! WRONG.');
+}
 
-// var questionLingual = prompt('Is english the only language that I speak?').toUpperCase();
-// console.log(questionLingual);
-// if(questionLingual === "NO" || questionLingual === "N") {
-//   alert('Richtig, I grew up speaking German and English!');
-// } else {
-//   alert('Wrong, bucko, ich spreche Deutsch');
-// } 
+var questionLingual = prompt('Is english the only language that I speak?').toUpperCase();
+console.log(questionLingual);
+if(questionLingual === "NO" || questionLingual === "N") {
+  questionCorrect++;
+  alert('Richtig, I grew up speaking German and English!');
+} else {
+  alert('Wrong, bucko, ich spreche Deutsch');
+} 
 
-// var questionSeason = prompt('Do I hate summer as a season?').toUpperCase();
-// console.log(questionSeason);
-// if(questionSeason === "YES" || questionSeason === "Y") {
-//   alert('Yes, the temperature of the earth is too damn high!');
-// } else {
-//   alert('Wrong, give me the Winter.');
-// }
+var questionSeason = prompt('Do I hate summer as a season?').toUpperCase();
+console.log(questionSeason);
+if(questionSeason === "YES" || questionSeason === "Y") {
+  questionCorrect++;
+  alert('Yes, the temperature of the earth is too damn high!');
+} else {
+  alert('Wrong, give me the Winter.');
+}
 
 var guessesRemaining = 4
 
@@ -94,4 +100,5 @@ while (statesRemaining > 0)  {
     }
 }
 
+alert('You guessed ' + questionCorrect + ' of 7 questions correct!')
 console.log(questionCorrect)
