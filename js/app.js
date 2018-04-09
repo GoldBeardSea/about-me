35374d1db24b89d5569cc3f890047c45773890f4
GoldbeardSea <timbusch1@gmail.com>
2018-04-08 22:49:18 -0700

    not beautiful, but functional, and ready to add more on

--- js/app.js
+++ js/app.js
@@ -3,75 +3,75 @@
 var questionCorrect = 0;
 
 var userName = prompt('Might I ask your name first?');
-alert('Alright ' + userName + ' it is time to play a game, please answer Yes/No or Y/n');
+alert('Alright ' + userName + ' it is time to play a game, please answer Yes/No or Y/N');
 
-// function callCat(){
-//   var questionCat = prompt('Do I love cats?').toUpperCase();
-//   console.log(questionCat);
-//   if(questionCat === 'YES' || questionCat === 'Y') {
-//     questionCorrect++;
-//     alert('Yes! I help foster and raise cats for adoption.');
-//   } else {
-//     alert('Smooth move, ExLax! Goose egg on this one.');
-//   }
-// }
+function callCat(){
+  var questionCat = prompt('Do I love cats?').toUpperCase();
+  console.log(questionCat);
+  if(questionCat === 'YES' || questionCat === 'Y') {
+    questionCorrect++;
+    alert('Yes! I help foster and raise cats for adoption.');
+  } else {
+    alert('Smooth move, ExLax! Goose egg on this one.');
+  }
+}
 
-// function callMusic(){
-//   var questionMusic = prompt('Do I make experimental music?').toUpperCase();
-//   console.log(questionMusic);
-//   if(questionMusic === 'YES' || questionMusic === 'Y') {
-//     questionCorrect++;
-//     alert('I friggen love playing the WEIRD stuff!');
-//   } else {
-//     alert('What do you think I am, the Monkees?!');
-//   }
-// }
+function callMusic(){
+  var questionMusic = prompt('Do I make experimental music?').toUpperCase();
+  console.log(questionMusic);
+  if(questionMusic === 'YES' || questionMusic === 'Y') {
+    questionCorrect++;
+    alert('I friggen love playing the WEIRD stuff!');
+  } else {
+    alert('What do you think I am, the Monkees?!');
+  }
+}
 
-// function callPeeps(){
-//   var questionPeeps = prompt('Do I love eating peeps?').toUpperCase();
-//   console.log(questionPeeps);
-//   if(questionPeeps === 'NO' || questionPeeps === 'N') {
-//     questionCorrect++;
-//     alert('Normal marshmallow is tasty enough, heck no to peeps!');
-//   } else {
-//     alert('Get out of town! WRONG.');
-//   }
-// }
-// console.log(questionCorrect);
+function callPeeps(){
+  var questionPeeps = prompt('Do I love eating peeps?').toUpperCase();
+  console.log(questionPeeps);
+  if(questionPeeps === 'NO' || questionPeeps === 'N') {
+    questionCorrect++;
+    alert('Normal marshmallow is tasty enough, heck no to peeps!');
+  } else {
+    alert('Get out of town! WRONG.');
+  }
+}
+console.log(questionCorrect);
 
-// function callGerman(){
-//   var questionLingual = prompt('Is english the only language that I speak?').toUpperCase();
-//   console.log(questionLingual);
-//   if(questionLingual === 'NO' || questionLingual === 'N') {
-//     questionCorrect++;
-//     alert('Richtig, I grew up speaking German and English!');
-//   } else {
-//     alert('Wrong, bucko, ich spreche Deutsch');
-//   }
-// }
-// console.log(questionCorrect);
+function callGerman(){
+  var questionLingual = prompt('Is english the only language that I speak?').toUpperCase();
+  console.log(questionLingual);
+  if(questionLingual === 'NO' || questionLingual === 'N') {
+    questionCorrect++;
+    alert('Richtig, I grew up speaking German and English!');
+  } else {
+    alert('Wrong, bucko, ich spreche Deutsch');
+  }
+}
+console.log(questionCorrect);
 
-// function callSeason(){
-//   var questionSeason = prompt('Do I hate summer as a season?').toUpperCase();
-//   console.log(questionSeason);
-//   if(questionSeason === 'YES' || questionSeason === 'Y') {
-//     questionCorrect++;
-//     alert('Yes, the temperature of the earth is too damn high!');
-//   } else {
-//     alert('Wrong, give me the Winter.');
-//   }
-// }
-// console.log(questionCorrect);
+function callSeason(){
+  var questionSeason = prompt('Do I hate summer as a season?').toUpperCase();
+  console.log(questionSeason);
+  if(questionSeason === 'YES' || questionSeason === 'Y') {
+    questionCorrect++;
+    alert('Yes, the temperature of the earth is too damn high!');
+  } else {
+    alert('Wrong, give me the Winter.');
+  }
+}
+console.log(questionCorrect);
 
-// function questionYesNo(){
-//   callCat();
-//   callMusic();
-//   callPeeps();
-//   callGerman();
-//   callSeason();
-// }
-// questionYesNo();
-// console.log(questionCorrect);
+function questionYesNo(){
+  callCat();
+  callMusic();
+  callPeeps();
+  callGerman();
+  callSeason();
+}
+questionYesNo();
+console.log(questionCorrect);
 
 var guessesRemaining = 4;
 
@@ -92,7 +92,7 @@ function callGear(){
       questionCorrect++;
       break;
     }
-    else if(guessesRemaining === 0) {
+    else if(parseInt(questionGear) !== 16 && guessesRemaining === 0) {
       alert('You are out of guesses.');
     }
   }
@@ -102,13 +102,11 @@ console.log(questionCorrect);
 
 var statesRemaining = 6;
 
-var statesLived = ['CALIFORNIA', 'VIRGINIA', 'TENNESSEE', 'GEORGIA', 'GERMANY', 'AFGHANISTAN'];
-var stateGuess = prompt('Can you guess a state or country where I have lived?').toUpperCase();
-var foundAnswer = false;
-console.log('state:', stateGuess);
-console.log(typeof stateGuess);
-
 function callStates(){
+  var statesLived = ['CALIFORNIA', 'VIRGINIA', 'TENNESSEE', 'GEORGIA', 'GERMANY', 'AFGHANISTAN'];
+  var foundAnswer = false;
+  console.log('state:', stateGuess);
+  console.log(typeof stateGuess);
   while (statesRemaining > 0) {
     var stateGuess = prompt('Can you guess a state or country where I have lived?').toUpperCase();
     statesRemaining--;
@@ -123,8 +121,11 @@ function callStates(){
       alert('Correct');
       questionCorrect++;
       break;
-    } else {
+    }
+    if (statesRemaining > 0) {
       alert('Try again');
+    } else if (statesRemaining === 0) {
+      alert('You are out of guesses');
     }
   }
 }
